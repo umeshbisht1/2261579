92cd290b7fe7a35681113ea954e9f34fb12e91b2
umesh bisht1 <umeshbisht792@gmail.com>
2025-07-15 11:40:00 +0530

    implemeted teh proxy server  in frontend to connectwith backend beacuse both are running in different server

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // adding proxy to frontend beacuse we cannnot direcly communicate with backend so we used as
-//  proxy server here my ncakend url  is which is running on my loal machin is http://localhost:3000
+//  proxy server here my ncakend url  is which is running on my local machine is http://localhost:3000
 export default defineConfig({
   plugins: [react()],
   server: {
